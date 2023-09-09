ce15368168a0c279d1889cfd52f3d32ea92a74bb
CapitaoDAreia <danieligord7@hotmail.com>
2023-09-09 14:13:41 -0300

    User entity and migration

---
+++ src/main/java/med/voll/api/domain/entities/User.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+}
