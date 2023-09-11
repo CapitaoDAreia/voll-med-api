7612d1ccaf86ee165d637a14a4d9afa0c7757641
CapitaoDAreia <danieligord7@hotmail.com>
2023-09-11 17:31:55 -0300

    Extracting subject from token

--- src/main/java/med/voll/api/application/services/TokenServices.java
+++ src/main/java/med/voll/api/application/services/TokenServices.java
@@ -1,8 +1,11 @@
 package med.voll.api.application.services;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import med.voll.api.domain.entities.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,7 +20,7 @@ public class TokenServices {
     @Value(value = "${api.security.token.secret}")
     private String secretKey;
 
-    public String generateToken(User user){
+    public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
             Instant expireDate = generateExpireDate();
@@ -28,12 +31,25 @@ public class TokenServices {
                     .withExpiresAt(expireDate)
                     .withClaim("id", user.getId())
                     .sign(algorithm);
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
             throw new RuntimeException("token creation failed: " + exception);
         }
     }
 
-    private Instant generateExpireDate(){
+    public String getTokenSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
+            return JWT.require(algorithm)
+                    .withIssuer("Voll API")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("invalid token: " + exception);
+        }
+    }
+
+    private Instant generateExpireDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
--- src/main/java/med/voll/api/infraestructure/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infraestructure/security/SecurityFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.application.services.TokenServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,12 +14,13 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenServices tokenServices;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = extractTokenFromRequestHeader(request);
-
-        System.out.println(token);
-
+        var tokenSubject = tokenServices.getTokenSubject(token);
         filterChain.doFilter(request, response);
     }
 
