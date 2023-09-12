389c1b41c26046d497f8afda0951fa547c7c7a7a
CapitaoDAreia <danieligord7@hotmail.com>
2023-09-12 15:15:00 -0300

    Implementing authentication

--- src/main/java/med/voll/api/infraestructure/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infraestructure/security/SecurityFilter.java
@@ -5,7 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import med.voll.api.application.services.TokenServices;
+import med.voll.api.domain.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,16 +21,27 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenServices tokenServices;
 
+    @Autowired
+    UsersRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = extractTokenFromRequestHeader(request);
-        var tokenSubject = tokenServices.getTokenSubject(token);
+
+        if (token != null) {
+            var tokenSubject = tokenServices.getTokenSubject(token);
+            UserDetails user = repository.findByLogin(tokenSubject);
+            var authentication = new UsernamePasswordAuthenticationToken(user.getUsername(), null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
         filterChain.doFilter(request, response);
     }
 
-    private String extractTokenFromRequestHeader(HttpServletRequest request){
+    private String extractTokenFromRequestHeader(HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader == null) throw new RuntimeException("token is missing");
-        return authorizationHeader.replace("Bearer ", "");
+        if (authorizationHeader != null) return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
--- src/main/java/med/voll/api/infraestructure/security/WebSecurityConfig.java
+++ src/main/java/med/voll/api/infraestructure/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package med.voll.api.infraestructure.security;
 
 import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,16 +13,25 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(@NotNull HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(@NotNull HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf().disable() //disable cross site request forgery protection policy
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //defines session management type
-                .and().build(); //build the config
+                .and().authorizeHttpRequests() //allows http request to be done
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth").permitAll() //defines which request are allowed
+                .anyRequest().authenticated() //defines that all requests, except allowed previously, are permitted only when authenticated
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) //defines that our custom filter comes before Spring filter
+                .build(); //build the config
     }
 
     @Bean
@@ -29,7 +40,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
