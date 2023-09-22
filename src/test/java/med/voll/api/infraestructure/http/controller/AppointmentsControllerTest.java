188252edc582bbdb53bfd87d774a37c1630fcc52
CapitaoDAreia <danieligord7@hotmail.com>
2023-09-21 21:16:02 -0300

    Writing tests for toCancel in AppointmentsControllerTest

--- src/test/java/med/voll/api/infraestructure/http/controller/AppointmentsControllerTest.java
+++ src/test/java/med/voll/api/infraestructure/http/controller/AppointmentsControllerTest.java
@@ -1,8 +1,14 @@
 package med.voll.api.infraestructure.http.controller;
 
 import med.voll.api.application.services.AppointmentServices;
+import med.voll.api.domain.dtos.appointments.CancelAppointmentDTO;
 import med.voll.api.domain.dtos.appointments.ScheduleAppointmentsDTO;
 import med.voll.api.domain.dtos.appointments.ScheduledAppointmentDTO;
+import med.voll.api.domain.entities.Address;
+import med.voll.api.domain.entities.Appointment;
+import med.voll.api.domain.entities.Doctor;
+import med.voll.api.domain.entities.Patient;
+import med.voll.api.domain.enums.CancelAppointmentReason;
 import med.voll.api.domain.enums.DoctorsExpertiseEnums;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -36,6 +42,9 @@ class AppointmentsControllerTest {
     @Autowired
     private JacksonTester<ScheduledAppointmentDTO> scheduledAppointmentDTO;
 
+    @Autowired
+    private JacksonTester<CancelAppointmentDTO> toCancelAppointmentDTO;
+
     @MockBean
     private AppointmentServices appointmentServices;
 
@@ -48,9 +57,35 @@ class AppointmentsControllerTest {
     }
 
     @Test
-    @DisplayName("Should return 200 when provided req body is existent and valid")
+    @DisplayName("Should return 400 when provided req body lack some information")
     @WithMockUser
     void toScheduleScenario2() throws Exception {
+        var date = LocalDateTime.now().plusHours(1);
+        var expertise = DoctorsExpertiseEnums.CARDIOLOGIA;
+        var toScheduleAppointment = new ScheduleAppointmentsDTO(2L, null, date, expertise);
+        var scheduledAppointment = new ScheduledAppointmentDTO(1L, 2L, 4L, date);
+
+        var toScheduleAppointmentDtoJson = scheduleAppointmentsDTO.write(toScheduleAppointment).getJson();
+
+        Mockito.when(appointmentServices.toSchedule(toScheduleAppointment)).thenReturn(scheduledAppointment);
+
+        var response = mvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .post("/api/v1/appointments")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(toScheduleAppointmentDtoJson)
+                )
+                .andReturn()
+                .getResponse();
+
+        Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Should return 200 when provided req body is existent and valid")
+    @WithMockUser
+    void toScheduleScenario3() throws Exception {
         var date = LocalDateTime.now().plusHours(1);
         var expertise = DoctorsExpertiseEnums.CARDIOLOGIA;
         var toScheduleAppointment = new ScheduleAppointmentsDTO(2L, 4L, date, expertise);
@@ -74,4 +109,68 @@ class AppointmentsControllerTest {
         Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
         Assertions.assertThat(response.getContentAsString()).isEqualTo(scheduledAppointmentDtoJson);
     }
+
+    @Test
+    @DisplayName("Should return 204 when a valid and existent appointment has cancelled")
+    @WithMockUser
+    void toCancelAppointmentScenario1() throws Exception {
+        //given
+        var now = LocalDateTime.now();
+        var appointment = new Appointment(1L, buildAnDoctor(), buildAnPatient(), now, true);
+        var toCancelAppointment = new CancelAppointmentDTO(appointment, CancelAppointmentReason.OTHER_REASON);
+
+        var toCancelAppointmentDtoJson = toCancelAppointmentDTO.write(toCancelAppointment).getJson();
+
+        Mockito
+                .doNothing()
+                .when(appointmentServices).toCancel(toCancelAppointment);
+
+        //when
+        var response = mvc.perform(
+                MockMvcRequestBuilders
+                        .delete("/api/v1/appointments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toCancelAppointmentDtoJson)
+        ).andReturn().getResponse();
+
+        //then
+        Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    @DisplayName("Should return 400 when provided DTO is invalid")
+    @WithMockUser
+    void toCancelAppointmentScenario2() throws Exception {
+        //given
+        var toCancelAppointment = new CancelAppointmentDTO(null, CancelAppointmentReason.OTHER_REASON);
+        var toCancelAppointmentDtoJson = toCancelAppointmentDTO.write(toCancelAppointment).getJson();
+
+        Mockito
+                .doNothing()
+                .when(appointmentServices).toCancel(toCancelAppointment);
+        //when
+        var response = mvc.perform(
+                MockMvcRequestBuilders
+                        .delete("/api/v1/appointments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toCancelAppointmentDtoJson)
+        ).andReturn().getResponse();
+
+        //then
+        Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+
+    private Address buildAnAddress() {
+        return new Address("here", "here", "06444900", "here", "sp", "12", "over here");
+    }
+
+    private Patient buildAnPatient() {
+        return new Patient(1L, "patient", "email@email.com", "123456", "12312312312", buildAnAddress(), true);
+    }
+
+    private Doctor buildAnDoctor() {
+        return new Doctor(1L, "doctor", "email", "1234567", "123456", DoctorsExpertiseEnums.CARDIOLOGIA, buildAnAddress(), true);
+    }
+
 }
